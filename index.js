2325038a0f7674496b0fdbfb7f2d6d29311557c2
Danni Wang <dw2730@nyu.edu>
2021-11-09 13:07:51 +0800

    update socket.io

--- index.js
+++ index.js
@@ -68,45 +68,45 @@ app.listen(port, () => {
     console.log('listening at ', port);
 });
 
-// //Initialize the express 'app' object
-// // let express = require('express');
-// // let app = express();
-// app.use('/', express.static('public'));
+//Initialize the express 'app' object
+// let express = require('express');
+// let app = express();
+app.use('/', express.static('public'));
 
-// //Initialize the actual HTTP server
-// let http = require('http');
-// let server = http.createServer(app);
-// let listenport = process.env.PORT || 5000;
-// server.listen(port, () => {
-//     console.log("Server listening at port: " + listenport);
-// });
+//Initialize the actual HTTP server
+let http = require('http');
+let server = http.createServer(app);
+let listenport = 5000;
+server.listen(listenport, () => {
+    console.log("Server listening at port: " + listenport);
+});
 
-// //Initialize socket.io
-// let io = require('socket.io');
-// io = new io.Server(server);
+//Initialize socket.io
+let io = require('socket.io');
+io = new io.Server(server);
 
-// //Listen for individual clients/users to connect
-// io.sockets.on('connection', function(socket) {
-//     console.log("We have a new client: " + socket.id);
+//Listen for individual clients/users to connect
+io.sockets.on('connection', function(socket) {
+    console.log("We have a new client: " + socket.id);
 
-//     //Listen for a message named 'msg' from this client
-//     socket.on('msg', function(data) {
-//         //Data can be numbers, strings, objects
-//         console.log("Received a 'msg' event");
-//         console.log(data);
+    //Listen for a message named 'msg' from this client
+    socket.on('msg', function(data) {
+        //Data can be numbers, strings, objects
+        console.log("Received a 'msg' event");
+        console.log(data);
 
-//         //Send a response to all clients, including this one
-//         io.sockets.emit('msg', data);
+        //Send a response to all clients, including this one
+        io.sockets.emit('msg', data);
 
-//         //Send a response to all other clients, not including this one
-//         // socket.broadcast.emit('msg', data);
+        //Send a response to all other clients, not including this one
+        // socket.broadcast.emit('msg', data);
 
-//         //Send a response to just this client
-//         // socket.emit('msg', data);
-//     });
+        //Send a response to just this client
+        // socket.emit('msg', data);
+    });
 
-//     //Listen for this client to disconnect
-//     socket.on('disconnect', function() {
-//         console.log("A client has disconnected: " + socket.id);
-//     });
-// });
+    //Listen for this client to disconnect
+    socket.on('disconnect', function() {
+        console.log("A client has disconnected: " + socket.id);
+    });
+});
